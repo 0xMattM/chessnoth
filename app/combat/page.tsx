66b81b18757d5e0ca64c07546261bc7b27dd00c9
Matt <166135184+0xMattM@users.noreply.github.com>
2025-12-09 19:57:31 -0300

    Delete .eslintrc.json completely to disable ESLint8

--- app/combat/page.tsx
+++ app/combat/page.tsx
@@ -1299,11 +1299,10 @@ export default function CombatPage() {
           }
           break
         }
-        case 'w': {
+        case 'w':
           event.preventDefault()
           handleAction('skill')
           break
-        }
         case '1': {
           event.preventDefault()
           handleAction('skill', 0)
